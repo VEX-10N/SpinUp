3c6c2bf8e0bee98df031827d9da3827e4c06b7b3
SecretAgent <iyengar.dhruv@outlook.com>
2023-02-02 15:20:32 -0800

    added pathfinding logic for gyro turns

--- src/movement.h
+++ src/movement.h
@@ -3,6 +3,23 @@
 
 using namespace vex;
 
+double calculateError(double initial, double desired) {
+  double error1 = desired - initial;
+  double error2 = 0;
+  if (initial > desired) {
+    error2 = (360 - initial) + desired;
+  } else if (initial < desired) {
+    error2 = (desired - 360) - initial;
+  } else {
+    return 0;
+  }
+  if (fabs(error1) < fabs(error2)) {
+    return error1;
+  } else {
+    return error2;
+  }
+}
+
 void resetMotors() {
   LeftFront.resetRotation();
   LeftBack.resetRotation();
@@ -39,12 +56,14 @@ void move_for(int distance, int speed,bool waitForCompletion) {
 }
 
 void turn_to(double desired) {
-  double error = fabs(desired - Inertial.heading(degrees));
+  double error = calculateError(Inertial.heading(degrees), desired);
   while (error > 2) {
-    error = fabs(desired - Inertial.heading(degrees));
+    error = calculateError(Inertial.heading(degrees), desired);
     double speed = error * 0.4;
     if (speed < 10) {
       speed = 10;
+    } else if (speed > -10) {
+      speed = -10;
     }
     LeftFront.spin(forward, speed, rpm);
     LeftBack.spin(forward, speed, rpm);
