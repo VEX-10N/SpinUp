78d424dca8fa6cea94670fc32420988b13abf2fc
SecretAgent <iyengar.dhruv@outlook.com>
2023-02-05 15:43:44 -0800

    added int to char method

--- src/util.h
+++ src/util.h
@@ -1,4 +1,4 @@
-void int_to_char(int integer) {
+char * int_to_char(int integer) {
   int n = integer;
   int digit = 0;
   while (integer > 0) {
@@ -6,5 +6,16 @@ void int_to_char(int integer) {
     n /= 10;
   }
   char* arr;
-  char* arr1 = char[digit + 1]();
+  char* arr1 = new char[digit + 1];
+  arr = new char[digit];
+  int index = 0;
+  while (n > 0) {
+    arr1[++index] = (char)(n % 10 + '0');
+    n /= 10;
+  }
+  int i = 0;
+  for (i = 0; i < index; i++) {
+    arr[i] = arr[index - i];
+  }
+  return arr;
 }
